e8e94d0be855a5a14306c95b4786eab037144d14
Tamir Bold <boldtamir@gmail.com>
2022-09-08 20:58:46 -0500

    Implemented

---
+++ lab3springdata/src/main/java/edu/miu/lab3springdata/Lab3SpringDataApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.lab3springdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3SpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab3SpringDataApplication.class, args);
+	}
+
+}
